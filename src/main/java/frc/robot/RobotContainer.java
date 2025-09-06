e27ff46ebd591507098ee60811cf19111db0e203
DragonKing-Dev <136646795+DragonKing-Dev@users.noreply.github.com>
2025-09-06 13:54:26 -0700

    Flywheel testbench

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,9 +8,17 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Flywheel;
+
+import static edu.wpi.first.units.Units.Volt;
+
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.util.Controller;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -21,34 +29,20 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Flywheel m_Flywheel = new Flywheel();
+  public double volt = 0.0;
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
-
+  private final Controller driverController = new Controller(0);
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
+  
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    driverController.triggerLeft().onTrue(m_Flywheel.setVelocityCommand(volt += 0.1));
   }
 
   /**
---
+++ src/main/java/frc/robot/subsystems/Flywheel.java
@@ -0,0 +1,113 @@
+package frc.robot.subsystems;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+ 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Flywheel extends SubsystemBase {
+    private final TalonFX flyWheelMotor;
+    private final TalonFXConfigurator flyWheelConfigurator;
+
+    private final VelocityVoltage velocityRequestFlywheel = new VelocityVoltage(0);
+
+    public TalonFXConfiguration motorConfigs;
+
+    private boolean enabled = false;
+    // private boolean velocityControl = true;
+
+    private double desiredVoltage = 0;
+    private final VelocityVoltage velocityRequest = new VelocityVoltage(0);
+    private final NeutralOut brakeRequest = new NeutralOut();
+
+    public Flywheel(){
+        flyWheelMotor = new TalonFX(0);
+        flyWheelConfigurator = flyWheelMotor.getConfigurator();
+        velocityRequest.Slot = 0;
+        motorConfigs = new TalonFXConfiguration();
+        configureMotor(motorConfigs);
+    }
+
+    public void configureMotor(TalonFXConfiguration motorConfigs) {
+        flyWheelConfigurator.refresh(motorConfigs);
+        // TODO change all of these values
+        motorConfigs.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+        motorConfigs.Voltage.PeakForwardVoltage = 11.5;
+        motorConfigs.Voltage.PeakReverseVoltage = -11.5;
+        motorConfigs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        // motorConfigs.MotorOutput.DutyCycleNeutralDeadband = RollerConstants.kNeutralDeadband;
+        motorConfigs.CurrentLimits.SupplyCurrentLimit = 40;
+        motorConfigs.CurrentLimits.SupplyCurrentLimitEnable = false;
+        motorConfigs.CurrentLimits.StatorCurrentLimit = 100;
+        motorConfigs.CurrentLimits.StatorCurrentLimitEnable = true;
+        motorConfigs.Audio.AllowMusicDurDisable = true;
+ 
+        flyWheelConfigurator.apply(motorConfigs);
+        // TODO add logging for errors
+
+        // StatusCode response = rollerConfigurator.apply(motorConfigs);
+        // if (!response.isOK())
+        //     DriverStation.reportError("Could not apply motor configs, error code:" + response.toString(), new Error().getStackTrace());
+        // rollerConfiguratorRight.refresh(motorConfigs);
+
+        // StatusCode responseRight = rollerConfiguratorRight.apply(motorConfigs);
+
+        // if (!responseRight.isOK())
+        // DriverStation.reportError("Could not apply motor configs, error code:" + responseRight.toString(), new Error().getStackTrace());
+    }
+
+    @Override
+    public void periodic(){
+        if (!enabled) return;
+        flyWheelMotor.setVoltage(desiredVoltage);
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+        
+        if (enabled) {
+            flyWheelMotor.setControl(velocityRequest);
+        } else {
+            desiredVoltage = 0.0;
+            flyWheelMotor.setControl(brakeRequest);
+        }
+    }
+
+    private void setVelocity(double velocity){
+        velocityRequestFlywheel.Velocity = velocity;
+    }
+
+    public double getTargetVelocity() {
+        return velocityRequestFlywheel.Velocity;
+    }
+
+    private void setVoltage(double volt) {
+        desiredVoltage = volt;
+    }
+
+    public Command setEnabledCommand(boolean enabled) {
+        return Commands.runOnce(() -> setEnabled(enabled));
+    }
+
+    public Command setVelocityCommand(double velocity){
+        return Commands.runOnce(() -> setVelocity(velocity));
+    }
+
+    public Command setVoltageCommand(double voltage){
+        return Commands.runOnce(() -> setVoltage(voltage));
+    }
+
+
+}
